c19e486e23926877cbce627189d606b90b2a200b
libosen <m@yonrd.com>
2024-10-02 21:13:03 +0800

    f

--- vpn/vpn.sh
+++ vpn/vpn.sh
@@ -252,8 +252,9 @@ install_pkgs() {
 
 get_setup_url() {
 #   base_url1="https://ghproxy.cn/https://raw.githubusercontent.com/hwdsl2/setup-ipsec-vpn/master"
-  base_url1="https://ghproxy.cn/https://raw.githubusercontent.com/PainterXIII/YouRan_Proxy/master/vpn"
-  base_url2="https://ghproxy.cn/https://gitlab.com/hwdsl2/setup-ipsec-vpn/-/raw/master"
+    base_url1="https://ghproxy.cn/https://raw.githubusercontent.com/PainterXIII/YouRan_Proxy/master/vpn"
+#   base_url2="https://ghproxy.cn/https://gitlab.com/hwdsl2/setup-ipsec-vpn/-/raw/master"
+    base_url2="base_url1="https://ghproxy.cn/https://raw.githubusercontent.com/PainterXIII/YouRan_Proxy/master/vpn""
   sh_file="vpnsetup_ubuntu.sh"
   if [ "$os_type" = "centos" ] || [ "$os_type" = "rhel" ] || [ "$os_type" = "rocky" ] \
     || [ "$os_type" = "alma" ] || [ "$os_type" = "ol" ]; then
